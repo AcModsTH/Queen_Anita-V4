6cf6d0ed579b6d458e138ac62672616dce6b0883
AcModsTH <acmodss2@gmail.com>
2024-12-12 18:08:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 global.owner = [
-  "2347043759577",
+  "212614389153",
   ""
 ]
-global.anitav4 = "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒"
-global.ownername = "David Cyril"
-global.packname = "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒"
-global.author = "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋"
-global.channelname = "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋"
+global.anitav4 = "Ac-Mods-𝐕𝟒"
+global.ownername = "AcMods"
+global.packname = "Ac-Mods-𝐕𝟒"
+global.author = "Ac-Mods"
+global.channelname = "Ac-Mods"
 global.channeljid = "120363315231436175@newsletter"
-global.autoTyping = false;                //make true to enable auto typing
+global.autoTyping = true;                //make true to enable auto typing
 global.autoRecord = false;                //make true to enable auto recording
 global.autoViewStatus = true;   // Set to true to automatically read statuses
 global.AUTOSTATUS_REACT = false;   // Set to true to automatically like statuses
@@ -17,7 +17,7 @@ global.autoLikeEmoji = "💚";    // Default emoji for liking statuses
 global.LEVELUP = false;
 
 
-global.ANTIVIEWONCE = false;
+global.ANTIVIEWONCE = true;
 
 global.roles = {
     1: "Novice",
@@ -37,7 +37,7 @@ global.sudoUsers = ["2349066528353@s.whatsapp.net", "2348129988915@s.whatsapp.ne
 global.public = true
 
 
-global.ANTIDELETE = false;  
+global.ANTIDELETE = true;  
 
 global.unavailable = true;     
 //to show your real presence  
@@ -54,7 +54,7 @@ global.prefix = '.';
 
 global.autobio = false;
 
-global.ANTICALL = false;
+global.ANTICALL = true;
 
 global.antilink = false;
 
